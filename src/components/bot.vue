702a4857c9c9cbe6dabfb6350dfc057ce469d663
Hery Ravelojaona <hery.ravelojaona@gmail.com>
2021-04-26 13:57:26 +0200

    Modal init style

---
+++ src/components/bot.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="bot">
+        <!-- Trigger/Open The Modal -->
+        <button id="myBtn">Une question ?</button>
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+            <div class="modal-header">
+                <button class="close">&times;</button>
+                <h2>Des questions ?</h2>
+            </div>
+            <div class="modal-content">
+                <p>Some text in the Modal..</p>
+            </div>
+            <div class="modal-footer">
+                <label for="question"></label>
+                <input type="text" name="question">
+                <button>
+                    <img alt="send question" src="../assets/filled-sent.png">
+                </button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Bot'
+}
+</script>
+
+<style lang="scss" scoped>
+#bot {
+    width: 300px;
+    height: auto;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    button {
+            border: none;
+            outline: none;
+            background-color: inherit;
+            cursor: pointer;
+    }
+    #myModal {
+        border: 1px solid;
+        border-radius: 3px;
+        box-shadow: 2px 2px 2px 2px  #fff;
+        margin:20px 0;
+        .close {
+            position: absolute;
+            top: 2px;
+            right: 10px;
+            font-size: 1.8em;
+        }
+    }
+    .modal-content {
+        min-height: 60vh;
+        padding: 10px;
+        background: #000;
+        color: #fff;
+    }
+    
+    .modal-header, .modal-footer {
+        position: relative;
+        background-color: #fff;
+        color: #000;
+        padding: 20px 0;
+    }
+
+    .modal-footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        input {
+            border: 1px solid;
+            height: 30px;
+            width: 80%;
+        }
+        img {
+            height: 30px;
+        }
+        
+    }
+
+}
+</style> scoped>
+
+</style>
